387e5034f5e0ddbd6de3337e1bdebcd9aad76a42
oscar9210 <mr.niceguy9210@gmail.com>
2024-11-17 22:19:31 +0800

    First Commit

--- src/main/java/com/Oscar/tugas3pbo/JFrame.java
+++ src/main/java/com/Oscar/tugas3pbo/JFrame.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
-package com.Auryn.tugas3pbo;
+package com.Oscar.tugas3pbo;
 import javax.swing.JOptionPane;
 /**
  *
